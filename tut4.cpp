5d7c2b905b583f0bee0ed511a9c530c393c9c34e
ASHISH SONI <91901271+ashishsoniii@users.noreply.github.com>
2021-10-31 17:28:49 +0530

    initial file

---
+++ tut4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int a, b,c;
+ cout<<"I am ashish (0/1) ? " <<endl;           cin>>a;
+ cout<<"I am soni (0/1) ? " <<endl;           cin>>b;
+
+cout << "I  am Ashish Soni   " <<(a*b);
+
+
+
+
+
+
+     return 0;
+}
